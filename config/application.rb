37c00039dbdb8f1ae21ba2bb672a0f9a9b17c2f1
Dee Meyers <dmeyers@spreedly.com>
2021-06-01 11:58:47 -0400

    env setup

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,11 @@ module SpreedlyAirlines
     # the framework and any gems in your application.
   end
 end
+
+# ENV
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
