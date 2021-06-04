b91cca4507cb2ea9abf51de86b8c7ed0d59ecdf9
Dee Meyers <dmeyers@spreedly.com>
2021-06-04 15:42:12 -0400

    added notes to self

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,37 +16,29 @@ class HomeController < ApplicationController
   def purchase
     @flight = Flight.find(params[:id])
     @amount = (@flight.price*100).to_i
+    # make gateway token if/else for expedia 
     @gateway_token = ENV['TEST_GATEWAY_TOKEN']
-    puts "imma gateway token: #{@gateway_token}" 
-    @envkey = ENV['ENV_KEY']
-    puts @envkey
-    @secret = ENV['ACCESS_SECRET']
-    puts @secret
     @token = params[:payment_method_token]
-    puts @token
+    # placeholder retain payment
+    @retain = true
+    # Below sends/recieves requests
     uri = URI.parse("https://core.spreedly.com/v1/gateways/#{@gateway_token}/purchase.json")
     request = Net::HTTP::Post.new(uri)
-    request.basic_auth("#{@envkey}", "#{@secret}")
+    request.basic_auth("#{ENV['ENV_KEY']}", "#{ENV['ACCESS_SECRET']}")
     request.content_type = "application/json"
     request.body = JSON.dump({
       "transaction" => {
         "payment_method_token" => "#{@token}",
         "amount" => @amount,
         "currency_code" => "USD",
-        "retain_on_success" => true
+        "retain_on_success" => @retain
       }
     })
-
     req_options = {
       use_ssl: uri.scheme == "https",
     }
-    puts request.body
-    
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-    puts response.body
-    
   end
-
 end
